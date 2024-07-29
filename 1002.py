a28c160d2e136033cfa9b583a800b402de434512
studentzzang <studentzzang@gmail.com>
2024-07-30 00:23:12 +0900

    done

--- 1002.py
+++ 1002.py
@@ -2,15 +2,23 @@ import math
 
 x = int(input())
 
-
 for i in range(x):
         
     x1,y1, r1, x2, y2, r2= map(float, input().split())
 
-    if x1==x2 and y1==y2 and r1==r2: # 위치, 반지름 같음
-        print(-1)
-    elif x1==x2 and y1==y2 and r1!=r2:  # 위치 같고 반지름 다름 (겹침 없음)
-        print(0)
+    dis = math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
-    elif math.sqrt(x2-x1)
+    if (dis == 0 and r1==r2): # 무한 만남 (중심같음, 반지름같음)
+        print(-1)
     
+    elif (dis == abs(r2-r1) or dis == abs(r2+r1)): # 내접 or 외접
+        print(1)
+
+    elif (dis > abs(r2-r1) and dis < abs(r2+r1)):
+        print(2)
+    
+    else:
+        print(0)
+    
+
+
